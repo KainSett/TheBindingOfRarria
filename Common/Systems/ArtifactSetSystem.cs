22657ccdace3fd1e77085cfc89c2a609f4e5dd0b
KainSett <7talessett@gmail.com>
2025-10-19 17:05:27 +0300

    almost done system

--- Common/Systems/ArtifactSetSystem.cs
+++ Common/Systems/ArtifactSetSystem.cs
@@ -1,8 +1,10 @@
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using Terraria.Localization;
+using Terraria.UI;
 using TheBindingOfRarria.Content.ArtifactSets;
 using TheBindingOfRarria.Content.Items;
 
@@ -11,6 +13,11 @@ namespace TheBindingOfRarria.Common.Systems;
 public class ArtifactSetSystem : ModSystem
 {
     public static List<ArtifactSet> ArtifactSets = [];
+
+    public override void Unload()
+    {
+        ArtifactSets.Clear();
+    }
 }
 
 public class ArtifactSet(LocalizedText name, Color color, Action<int> effect, params Predicate<int>[] conditions)
@@ -25,11 +32,18 @@ public class ArtifactSet(LocalizedText name, Color color, Action<int> effect, pa
 
     public void Update(int who, params int[] items)
     {
+        if (!Main.player[who].TryGetModPlayer<ArtifactSetPlayer>(out var p))
+            return;
+
         foreach (var item in Artifacts)
         {
             if (!items.Any(i => item(i)))
-                return;
+                continue;
+            else 
+                p.Sets[this] += 1;
         }
+        if (p.Sets[this] < 3)
+            return;
 
         Effect.Invoke(who);
     }
@@ -38,11 +52,13 @@ public class ArtifactSet(LocalizedText name, Color color, Action<int> effect, pa
     {
         return Artifacts.Any(art => art.Invoke(type));
     }
+
+    public int Count => Artifacts.Count;
 }
 
 public class ArtifactSetPlayer : ModPlayer
 {
-    public List<ArtifactSet> Sets = ArtifactSetSystem.ArtifactSets;
+    public Dictionary<ArtifactSet, int> Sets = [];
 
     public List<int> Artifacts = [];
 
@@ -50,7 +66,22 @@ public class ArtifactSetPlayer : ModPlayer
     {
         foreach(var set in Sets)
         {
-            set.Update(Player.whoAmI, [..Artifacts]);
+            set.Key.Update(Player.whoAmI, [..Artifacts]);
+        }
+    }
+
+    public override void PreUpdate()
+    {
+        Artifacts.Clear();
+        foreach (var set in Sets)
+            Sets[set.Key] = 0;
+    }
+
+    public override void Initialize()
+    {
+        foreach (var set in ArtifactSetSystem.ArtifactSets)
+        {
+            Sets.Add(set, 0);
         }
     }
 }
@@ -70,10 +101,27 @@ public class Artifact : GlobalItem
         if (Main.LocalPlayer.TryGetModPlayer<ArtifactSetPlayer>(out var p))
         {
             foreach (var set in p.Sets)
-                if (set.Contains(item.type))
+                if (set.Key.Contains(item.type))
                 {
-                    var shift = Main.keyState.IsKeyDown(Keys.LeftShift) || Main.keyState.IsKeyDown(Keys.RightShift) ? "Open" : "Closed";
-                    var t = new TooltipLine(Mod, "ArtifactSet", $"[c/{set.NameColor.Hex4()}:" + Language.GetOrRegister($"Mods.{Mod.Name}.ArtifactSets.{set.Name}.Name").Value + ']' + Language.GetOrRegister($"Mods.{Mod.Name}.ArtifactSets.Info.{shift}").Value);
+                    var shift = Language.GetTextValue($"Mods.TheBindingOfRarria.ArtifactSets.Info.Closed");
+                    var name = $"[c/{set.Key.NameColor.Hex3()}:" + Language.GetTextValue($"Mods.TheBindingOfRarria.ArtifactSets.{set.Key.Name}.Name") + ']';
+                    if (Main.keyState.IsKeyDown(Keys.LeftShift) || Main.keyState.IsKeyDown(Keys.RightShift))
+                    {
+                        shift = $" ({set.Value}/{set.Key.Count})";
+
+                        var line1 = name + shift;
+
+                        var tooltip = new TooltipLine(Mod, "ArtifactSetEffect", Language.GetTextValue($"Mods.TheBindingOfRarria.ArtifactSets.{set.Key.Name}.Effect"));
+                        var n = new TooltipLine(Mod, "ArtifactSet", line1);
+                        tooltips.Insert(1, tooltip);
+                        tooltips.Insert(1, n);
+                        for (int i = 3; i < tooltips.Count; i++)
+                            tooltips[i].Hide();
+                        return;
+                    }
+                    var text = name + shift;
+
+                    var t = new TooltipLine(Mod, "ArtifactSet", text);
 
                     tooltips.Insert(1, t);
                 }
